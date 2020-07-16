d5486aaf2e6097b328469c4dd35977bc2cbed4d4
Pablo2e <hebenstreitpablo@hotmail.com>
2020-07-16 17:10:45 +0200

    cambios por push

---
+++ angular-silenole/src/app/serv.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServService {
+
+  public aparecer:boolean
+  
+  constructor() {
+    console.log('funcionando servicio') 
+    this.aparecer=false
+  }
+  
+}
