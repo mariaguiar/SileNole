aaf1f96991863b53408191952ef4afb6627a1442
Pablo2e <hebenstreitpablo@hotmail.com>
2020-07-16 08:35:55 +0200

    header alternativo

--- angular-silenole/src/app/app.module.ts
+++ angular-silenole/src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { UploadComponent } from './pages/upload/upload.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    UploadComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-silenole/src/app/pages/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  collapsed = true;
+  toggleCollapsed(): void {
+  this.collapsed = !this.collapsed;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
+
+
