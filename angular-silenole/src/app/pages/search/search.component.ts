2dcf39e8dc3943e55139ec52097403b04a1087b9
María Aguiar <65535353+mariaguiar@users.noreply.github.com>
2020-07-17 00:34:11 +0000

    Update search.component.ts

--- angular-silenole/src/app/pages/search/search.component.ts
+++ angular-silenole/src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {ServService} from './../../serv.service'
 
 @Component({
   selector: 'app-search',
@@ -13,9 +15,33 @@ export class SearchComponent implements OnInit {
     
   }
 
-  constructor() { }
+  closeResult = '';
 
-  ngOnInit(): void {
-  }
+  constructor(public servicio:ServService,private modalService: NgbModal) { }
+
+  public aparecerF(){
+    this.servicio.aparecer=true
+    console.log(this.servicio.aparecer)
+    }
+  
+    ngOnInit(): void {
+    }
+    open(content4) {
+      this.modalService.open(content4, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `${this.getDismissReason(reason)}`;
+      });
+    }
+    
+  
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      }  else {
+        return '';
+      }
+    }
+  
 
 }
