59408c523524bffca3cc6d793dbddc1ccde7967b
Pablo2e <hebenstreitpablo@hotmail.com>
2020-07-20 11:01:44 +0200

    Cambio botones y agregados los modales de cierre

--- angular-silenole/src/app/pages/header/header.component.ts
+++ angular-silenole/src/app/pages/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -13,9 +14,24 @@ export class HeaderComponent implements OnInit {
     
   }
 
-  constructor() { }
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
-
+  open4(content4) {
+    this.modalService.open(content4, {ariaLabelledBy: 'modalCerrarSesion'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    }  else {
+      return '';
+    }
+  }
 }
--- angular-silenole/src/app/pages/profile/profile.component.ts
+++ angular-silenole/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {ServService} from './../../serv.service'
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
-
+  open3(content3) {
+    this.modalService.open(content3, {ariaLabelledBy: 'modalEliminarCuenta'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    }  else {
+      return '';
+    }
+  }
 }
