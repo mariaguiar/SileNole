512c5404fa8374f0090776f97a6169172d53f42b
Cesar <Cesar24gas@gmail.com>
2020-07-27 10:10:24 +0200

    Solucion de Error en shared

--- angular-silenole/src/app/shared/usuario.service.ts
+++ angular-silenole/src/app/shared/usuario.service.ts
@@ -1,43 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Usuario } from './../models/usuario';
+import { login } from '../models/user.login';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-
   public usuario: Usuario;
-  public idUsuario: number;
-  
-  private url = "http://localhost:3000/"
-  
+  private url = "http://localhost:3000/user/register"
   constructor(private http: HttpClient) { }
 
-  public autentificado:boolean = false
-  
-  getUsuario(id: number){
-    if (!id){
-      return this.http.get(this.url + "user/register")
-    }else{
-      return this.http.get(this.url + "user/" + id)
-    }
-  }
+
   newUsuario(nuevoUsuario: Usuario){
     console.log(this.url)
-    return this.http.post(this.url + "user/register", nuevoUsuario)
-  }
-  putUsuario(cambios: Usuario){
-    return this.http.put(this.url + "user", cambios)
-  }
-  deleteUsuario(id: number){
-    const options = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-      body: {
-        user_id: id,
-      },
-    };
-    return this.http.delete(this.url + "user/", options)
+    return this.http.post(this.url, nuevoUsuario)
   }
 }
+
+ 
